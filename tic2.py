b365a6db64c6c75d504ff025d7610e5d17262b40
Nathan Ning <nathanning70@gmail.com>
2023-05-19 20:01:10 -0500

    Start new version of tic tac toe, using class

---
+++ tic2.py
@@ -0,0 +1,7 @@
+# Revised version of tic tac toe, using a 2 dimensional array instead of lists
+# 2 dimensional array part of new class
+
+class TicTacToe:
+    def __init__(self):
+        self.grid = []
+    
