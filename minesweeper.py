206c50c645bb5d552262fd3ed15a6c6c1b2b82a5
Nathan Ning <nathanning70@gmail.com>
2023-09-06 19:44:09 -0500

    Input for move, Game Over condition

--- minesweeper.py
+++ minesweeper.py
@@ -8,6 +8,7 @@ class minesweeper:
         if width < 1 or height < 1 or mines < 1:
             print("Invalid Parameters: Width, Height, and Mine Count >= 1")
             raise
+        self.ongoing = True
         self.width = width
         self.height = height
         self.mines = mines
@@ -33,12 +34,11 @@ class minesweeper:
                                 pass
 
         self.shown = [[-2 for x in range(height)] for x in range(width)]
+        while self.ongoing:
+            self.move()
 
 
     def mark_mine(self, x, y):
-        if x < 0 or y < 0 or x >= self.width or y >= self.height:
-            print("Invalid Input")
-            return
         temp = self.shown[x][y]
         if temp > -2:
             print("Invalid Input")
@@ -53,11 +53,33 @@ class minesweeper:
         pass
 
     def move(self):
-        pass
-        
-        
+        try:
+            x = int(input("X Coordinate From Left? Pick a number from '1' to the width: "))
+            y = int(input("Y Coordinate From Top? Pick a number from '1' to the height: "))
+            ifmark = int(input("Mark or Select? If Mark, type '1'. If Select, Type '2'. "))
+            if x < 0 or y < 0 or x >= self.width or y >= self.height or ifmark < 1 or ifmark > 2:
+                print("Invalid Input")
+                return
+        except:
+            print("Invalid Input.")
+            return
+        print(x, y, ifmark)
+        print(self.shown)
+        if ifmark == 1:
+            self.mark_mine(x,y)
+            self.show_shown()
+        if ifmark == 2:
+            temp = self.shown[x][y]
+            if temp != -2:
+                print("Invalid Input")
+                return
+            temp = self.grid[x][y]
+            if temp == -1:
+                raise GameOver
 
-
+        
+class GameOver(Exception):
+    pass
 
 def play(width = 10, height = 10, mines = 15):
     board = minesweeper(width, height, mines)
@@ -65,9 +87,13 @@ def play(width = 10, height = 10, mines = 15):
 resume = True
 while resume == True:
     try:
-        play(int(input("Board Width? ")), int(input("Board Height?" )), int(input("Mine Count? ")))
+        play(int(input("Board Width? ")), int(input("Board Height? ")), int(input("Mine Count? ")))
         resume = False
     except TypeError:
         print("Input an Integer")
+    except GameOver:
+        print("Boom. You Lose")
+        if input("Type 'Continue' to try again. ") != "Continue":
+            resume = False
     except:
-        pass
+        print("Unexpected Error")
