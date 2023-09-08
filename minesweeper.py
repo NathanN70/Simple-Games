ef8222af0e08a9ad488a24b2bb73129f4f077a14
Nathan Ning <nathanning70@gmail.com>
2023-09-08 12:20:52 -0500

    comments, reduced unused variables

--- minesweeper.py
+++ minesweeper.py
@@ -8,7 +8,6 @@ class minesweeper:
         if width < 1 or height < 1 or mines < 1:
             print("Invalid Parameters: Width, Height, and Mine Count >= 1")
             raise
-        self.ongoing = True
         self.width = width
         self.height = height
         self.mines = mines
@@ -21,9 +20,10 @@ class minesweeper:
             x = spot % width
             y = spot // width
             self.grid[y][x] = -1
-
+        # place numbers in tiles neighboring mines
         for x in range(self.width):
             for y in range(self.height):
+                # prevent negative indexing in lists affecting numbers
                 if self.grid[y][x] == 0:
                     for checkx in range(-1, 2):
                         if x + checkx < 0:
@@ -36,12 +36,13 @@ class minesweeper:
                                     self.grid[y][x] += 1
                             except:
                                 pass
-
+        # grid visible to players, default every spot is "?"
         self.shown = [["?" for x in range(width)] for x in range(height)]
-        while self.ongoing:
+        # player prompted to move indefinitely until "GameOver" error, which happens when player wins or loses
+        while True:
             self.move()
 
-
+    # player marks mines to indicate an uncovered mine, can be done to any uncovered tile
     def mark_mine(self, x, y):
         temp = self.shown[y][x]
         if temp != "?" and temp != "!":
@@ -51,9 +52,9 @@ class minesweeper:
             self.shown[y][x] = "!"
         else:
             self.shown[y][x] = "?"
-    
+    # show revealed board to player, including marked mines
     def show_shown(self):
-        print(" X |", end = "")
+        print(" X ", end = "|")
         for x in range(self.width):
             print(str(x + 1).center(3), end = "")
         print("\n----" + ("---" * self.width))
@@ -68,9 +69,8 @@ class minesweeper:
             x = int(input("X Coordinate From Left? Pick a number from '1' to the width: ")) - 1
             y = int(input("Y Coordinate From Top? Pick a number from '1' to the height: ")) - 1
             ifmark = int(input("Mark or Select? If Mark, type '1'. If Select, Type '2'. "))
-            if x < 0 or y < 0 or x >= self.width or y >= self.height or ifmark < 1 or ifmark > 2:
-                print("Invalid Input")
-                return
+            if x < 0 or y < 0 or x >= self.width or y >= self.height or ifmark not in [1, 2]:
+                raise
         except:
             print("Invalid Input.")
             return
@@ -94,7 +94,7 @@ class minesweeper:
                 print("You Win!")
                 raise GameOver
             
-    
+    # When a zero is found, auto clear neighbor tiles
     def implicit_move(self, x, y):
         for hor in range(-1, 2):
             if hor + x < 0:
@@ -120,7 +120,7 @@ class minesweeper:
                     return False
         return True
 
-        
+# invoked when player wins or loses
 class GameOver(Exception):
     pass
 
@@ -135,4 +135,4 @@ while resume == True:
         if input("Type 'Continue' to play again. ") != "Continue":
             resume = False
     except:
-        print("Unexpected Error")
+        pass
