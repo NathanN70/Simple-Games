5e313bf5c79eed8c926a04d1d07cd0691e159d0f
Nathan Ning <nathanning70@gmail.com>
2023-09-11 15:24:39 -0500

    prop management, roll d6 function

--- monopoly/monopoly.py
+++ monopoly/monopoly.py
@@ -5,6 +5,8 @@ class monopoly:
         self.playercount = playercount
         self.players = [player(x + 1) for x in range(playercount)]
         random.seed()
+        self.board = []
+        # initialize board
 
     def move(self, currentplayer):
         print("Player " + str(currentplayer.position) + " Turn")
@@ -12,8 +14,7 @@ class monopoly:
             self.jailmove(currentplayer)
             return
         print("Rolling...")
-        firstdie = random.randint(1, 6)
-        seconddie = random.randint(1, 6)
+        firstdie, seconddie = d6(), d6()
         doubles = 1
         print(str(firstdie) + ", " + str(seconddie))
         # pass go check
@@ -24,8 +25,7 @@ class monopoly:
         while firstdie == seconddie:
             print("Doubles! Go Again!")
             print("Rolling...")
-            firstdie = random.randint(1, 6)
-            seconddie = random.randint(1, 6)
+            firstdie, seconddie = d6(), d6()
             print(str(firstdie) + ", " + str(seconddie))
             if firstdie == seconddie:
                 doubles += 1
@@ -38,17 +38,28 @@ class monopoly:
             print("Speeding! Go to Jail!")
             currentplayer.location = 10
             currentplayer.jailed = 3
+        if input("Any more actions?(houses, mortgages) Type 'Yes' if so. ") == 'Yes':
+            action = input("What action would you like to take? Type '1' to manage houses, Type '2' to manage mortgages. ")
+            while action != '1' and action != '2':
+                if action == '1':
+                    self.managehouses(currentplayer)
+                if action == '2':
+                    self.managemortgages(currentplayer)
+                action = input("Any more actions? Type '1' to manage houses, Type '2' to manage mortgages. ")
+                
         # prompt actions (build houses, manage mortgages, etc.)
         # next player's turn
         
 
         
     def checkgopass(self, currentplayer, roll):
-        if currentplayer.location + roll >= 40:
+        if currentplayer.location + roll >= self.board(len):
             currentplayer.money += 200
-            currentplayer.location -= 40
+            currentplayer.location -= self.board(len)
         
-        
+    def buyprop(self, player, prop):
+        pass
+
         
 
     def jailmove(self, currentplayer):
@@ -91,10 +102,33 @@ class monopoly:
         if player.money >= 0:
             return
         # get more money through selling/mortgaging properties
+    
+    def managehouses(self, player):
+        managing = True
+        while managing:
+            for prop in player.property:
+                print(prop, prop.houses)
+            tomanage = input("What property do you want to manage the houses of? ")
+            # management
+            moremanaging = input("Do you want to manage more houses? If so, type '1'")
+            if moremanaging != '1':
+                managing = False
+
+    def managemortgages(self, player):
+        managing = True
+        while managing:
+            for prop in player.property:
+                print(prop, prop.houses)
+            tomanage = input("What property do you want to manage the houses of? ")
+            # management
+            moremanaging = input("Do you want to manage more houses? If so, type '1'")
+            if moremanaging != '1':
+                managing = False
         
         
-            
-            
+def d6():
+    return random.randint(1, 6)
+
 
 class player:
     def __init__(self, position):
@@ -128,6 +162,30 @@ class rail(property):
     pass
 
 class norm(property):
+    pass
+
+class normA(norm):
+    pass
+
+class normB(norm):
+    pass
+
+class normC(norm):
+    pass
+
+class normD(norm):
+    pass
+
+class normE(norm):
+    pass
+
+class normF(norm):
+    pass
+
+class normG(norm):
+    pass
+
+class normH(norm):
     pass
 
 class GameOver(Exception):
