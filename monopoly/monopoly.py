57279be8ceb4e0724967217fb95016c8e9097190
Nathan Ning <nathanning70@gmail.com>
2023-09-14 22:00:15 -0500

    naming consistency, move loop, syntax error fixes

--- monopoly/monopoly.py
+++ monopoly/monopoly.py
@@ -3,7 +3,9 @@ import monopolyboard as board_to_use
 
 class monopoly:
     def __init__(self, playercount):
-        self.playercount = playercount
+        self.players = []
+        for x in range(1, playercount + 1):
+            self.players.append(player(input("Player " + str(x) + " Name? ")))
         self.players = [player(x + 1) for x in range(playercount)]
         random.seed()
         self.board = []
@@ -22,12 +24,27 @@ class monopoly:
             if squaretype in ["A", "B", "C", "D", "E", "F", "G", "H"]:
                 self.board.append(norm(square[0], square[1], square[2], square[3], square[4]))
         # initialize board
+        self.losers = []
+        for x in range(len(self.players)):
+            if self.players[x] not in self.losers:
+                self.move(self.players[x])
+            if len(self.board) - len(self.losers) == 1:
+                break
+        for x in self.players:
+            if x not in self.losers:
+                print("Congragulations " + str(x.name))
+        print("Congragulations " + str(self.players[0]))
 
     def move(self, currentplayer):
-        print("Player " + str(currentplayer.position) + " Turn")
+        print("Player " + str(currentplayer.name) + " Turn")
         if currentplayer.jailed >= 0:
             self.jailmove(currentplayer)
-            return
+            if currentplayer.jailed > 0:
+                currentplayer.jailed -= 1
+                return
+            if currentplayer.jailed == -2:
+                currentplayer.jailed = -1
+                return
         print("Rolling...")
         firstdie, seconddie = d6(), d6()
         doubles = 1
@@ -36,7 +53,7 @@ class monopoly:
         self.checkgopass(currentplayer, firstdie + seconddie)
         currentplayer.location += firstdie + seconddie
         # new location effect
-        self.locationeffect(player)
+        self.locationeffect(currentplayer)
 
         while firstdie == seconddie:
             print("Doubles! Go Again!")
@@ -49,10 +66,10 @@ class monopoly:
                     break
             self.checkgopass(currentplayer, firstdie + seconddie)
             currentplayer.location += firstdie + seconddie
-            self.locationeffect(player)
+            self.locationeffect(currentplayer)
         if doubles == 3:
             print("Speeding! Go to Jail!")
-            currentplayer.location = 10
+            currentplayer.location = 0
             currentplayer.jailed = 3
         if input("Any more actions?(houses, mortgages) Type 'Yes' if so. ") == 'Yes':
             action = input("What action would you like to take? Type '1' to manage houses, Type '2' to manage mortgages. ")
@@ -62,51 +79,62 @@ class monopoly:
                 if action == '2':
                     self.managemortgages(currentplayer)
                 action = input("Any more actions? Type '1' to manage houses, Type '2' to manage mortgages. ")
-                
+        
+        if self.players[-1] != currentplayer:
+            self.move(self.players[self.players.index(currentplayer) + 1])
+        else:
+            self.move(self.players[0])                
         # prompt actions (build houses, manage mortgages, etc.)
         # next player's turn
         
 
         
     def checkgopass(self, currentplayer, roll):
-        if currentplayer.location + roll >= self.board(len):
+        if currentplayer.location + roll >= len(self.board):
             currentplayer.money += 200
-            currentplayer.location -= self.board(len)
+            currentplayer.location -= len(self.board)
         
-    def buyprop(self, player, prop):
-        self.takemoney(player, prop.buy_price)
-        player.property.append(prop)
-        prop.owner = player
+    def buyprop(self, currentplayer, prop):
+        self.takemoney(currentplayer, prop.buy_price)
+        currentplayer.property.append(prop)
+        prop.owner = currentplayer
 
-    def locationeffect(self, player):
-        newlocation = self.board[player]
+    def locationeffect(self, currentplayer):
+        print(currentplayer.location)
+        newlocation = self.board[currentplayer.location]
         print(newlocation.name)
         if isinstance(newlocation, free):
             return
-        if isinstance(newlocation, property):
-            self.landprop(player)
+        if isinstance(newlocation, property_class):
+            self.landprop(currentplayer, self.board[currentplayer.location])
             return
         if isinstance(newlocation, chest):
-            self.landchest(player)
+            self.landchest(currentplayer)
             return
         if isinstance(newlocation, chance):
-            self.landchance(player)
+            self.landchance(currentplayer)
 
-    def landprop(self, player, prop):
+    def landprop(self, currentplayer, prop):
         if prop.owner == 0:
             tobuy = input("Do you want to buy it? If so, type '1'")
             if tobuy == 1:
-                self.buyprop(player, prop)
+                self.buyprop(currentplayer, prop)
 
-    def landchest(self, player):
+    def landchest(self, currentplayer):
         pass
 
-    def landchance(self, player):
+    def landchance(self, currentplayer):
         pass
 
         
 
     def jailmove(self, currentplayer):
+        print("first")
+        if currentplayer.jailed == 0:
+            self.takemoney(currentplayer, 50)
+            currentplayer.jailed = -1
+            return
+        print("second")
         if currentplayer.gooj != 0:
             print(str(currentplayer.jailed) + " Turns remaining in jail.")
             usegooj = input("Do you want to use a Get Out of Jail Free card? If yes, type 'Yes'. If not type anything else. ")
@@ -119,26 +147,28 @@ class monopoly:
                     else:
                         currentplayer.gooj -= 2
                         #put gooj in chance discard
-                currentplayer.jailed == -1
-                self.move(currentplayer)
+                else:
+                    currentplayer.gooj = 0
+                currentplayer.jailed = -1
                 return
-        if currentplayer.jailed == 0:
+        print("third")
+        if input("Player " + str(currentplayer.jailed) + " buy out of jail for $50? Type 'Yes' to buy out. ") == 'Yes':
             self.takemoney(currentplayer, 50)
-            return
-        if 'Yes' == input("Player " + str(currentplayer.position) + " buy out of jail for $50? Type 'Yes' to buy out. "):
-            self.takemoney(currentplayer, 50)
+            currentplayer.jailed = -1
             return
         print("Rolling...")
-        firstdie = random.randint(1, 6)
-        seconddie = random.randint(1, 6)
+        firstdie = d6()
+        seconddie = d6()
         print(str(firstdie) + ", " + str(seconddie))
         if firstdie == seconddie:
             print("Doubles! Break out of Jail! (No Additional Move)")
-        currentplayer.location += firstdie + seconddie
-        self.locationeffect(player)
+            currentplayer.jailed == -2
+            currentplayer.location += firstdie + seconddie
+            self.locationeffect(currentplayer)
+            return
         if input("Any more actions?(houses, mortgages) Type 'Yes' if so. ") == 'Yes':
             action = input("What action would you like to take? Type '1' to manage houses, Type '2' to manage mortgages. ")
-            while action != '1' and action != '2':
+            while action == '1' or action == '2':
                 if action == '1':
                     self.managehouses(currentplayer)
                 if action == '2':
@@ -148,16 +178,16 @@ class monopoly:
         
             
     
-    def takemoney(self, player, amount):
-        player.money -= amount
-        if player.money >= 0:
+    def takemoney(self, currentplayer, amount):
+        currentplayer.money -= amount
+        if currentplayer.money >= 0:
             return
         # get more money through selling/mortgaging properties
     
-    def managehouses(self, player):
+    def managehouses(self, currentplayer):
         managing = True
         while managing:
-            for prop in player.property:
+            for prop in currentplayer.property:
                 print(prop, prop.houses)
             tomanage = input("What property do you want to manage the houses of? ")
             # management
@@ -165,10 +195,10 @@ class monopoly:
             if moremanaging != '1':
                 managing = False
 
-    def managemortgages(self, player):
+    def managemortgages(self, currentplayer):
         managing = True
         while managing:
-            for prop in player.property:
+            for prop in currentplayer.property:
                 print(prop, prop.houses)
             tomanage = input("What property do you want to manage the houses of? ")
             # management
@@ -178,17 +208,17 @@ class monopoly:
         
         
 def d6():
-    return random.randint(1, 6)
+    return random.randint(1, 2)
 
 
 class player:
-    def __init__(self, turn_order):
+    def __init__(self, name):
+        self.name = name
         self.money = 1500
         # get out of jail free cards
         self.gooj = 0
         self.property = []
         self.location = 0
-        self.turn_order = turn_order
         self.jailed = -1
 # location id based on code system. Letter, then Number. Normal props, Letters A-H each represent a district. The number (1-3) represents different props in the district
 # for free spaces, letter X, number system same
@@ -211,22 +241,22 @@ class chance(location):
     def __init__(self, id, name):
         super().__init__(id, name)
 
-class property(location):
+class property_class(location):
     def __init__(self, id, name, buy_price, prices):
         self.buy_price = buy_price
         self.prices = prices
-        self.owner = 0
+        self.owner = None
         super().__init__(id, name)
 
-class util(property):
+class util(property_class):
     def __init__(self, id, name, buy_price, prices):
         super().__init__(id, name, buy_price, prices)
 
-class rail(property):
+class rail(property_class):
     def __init__(self, id, name, buy_price, prices):
         super().__init__(id, name, buy_price, prices)
 
-class norm(property):
+class norm(property_class):
     def __init__(self, id, name, buy_price, prices, house_cost):
         self.house_cost = house_cost
         super().__init__(id, name, buy_price, prices)
